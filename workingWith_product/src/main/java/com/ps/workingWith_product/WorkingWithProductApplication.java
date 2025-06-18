e5d9ac8aae365b8b843fa653f2de28a2dd8eaf67
Thierno <podiallo2242@gmail.com>
2025-06-18 08:45:58 -0400

    First commit

---
+++ workingWith_product/src/main/java/com/ps/workingWith_product/WorkingWithProductApplication.java
@@ -0,0 +1,13 @@
+package com.ps.workingWith_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingWithProductApplication.class, args);
+	}
+
+}
